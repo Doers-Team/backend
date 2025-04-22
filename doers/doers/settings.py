6cece2c855e263e8a2e74cb01e8e9ea08cbf2efd
kiripupsik5 <kirillpup227@gmail.com>
2025-04-22 13:34:49 +0300

    Connect Postgresql

--- doers/doers/settings.py
+++ doers/doers/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'doers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'doers_db',
+        'USER': 'kiripupsik',
+        'PASSWORD': 'kiriLLpup565567',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
